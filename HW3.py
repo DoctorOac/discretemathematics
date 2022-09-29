b841e2c4354f50371a777f1b671c9216b41d048e
Coak <oakenshieldc@student.swosu.edu>
2022-09-29 12:40:24 -0500

    created HW3

---
+++ HW3.py
@@ -0,0 +1,11 @@
+
+'''What are the quotient and remainder when:
+a) 19 divided by 7
+b)-111 divided by 11
+c)789 divided by 23
+d)1001 divided by 13?
+e)0 divided by 19
+f)3 divided by 5
+g) -1 divided by 3?
+h)4 divided by one'''
+
